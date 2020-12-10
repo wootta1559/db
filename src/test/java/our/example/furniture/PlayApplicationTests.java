d6d924e2327a0abb7bc240546ce5cdd89cc7d683
이지훈 <wootta1559@naver.com>
2020-12-10 22:53:15 +0900

    회원가입프론트

---
+++ src/test/java/our/example/furniture/PlayApplicationTests.java
@@ -0,0 +1,13 @@
+package our.example.furniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
